87ed3b5e34ddc19ef9e79e1684849ff327475f36
Abdislam <sultangabaxey@gmail.com>
2024-11-17 19:16:51 +0300

    Basic Of Python

---
+++ lessons.py
@@ -0,0 +1,153 @@
+# # #  Assigning values to variabiles
+
+# # counter = 100 # an integer assignment
+# # miles = 1000.0 # an floating assignment
+# # name = "johan" # an string assignment
+
+# # #   multile Assignment
+
+# # a=b=c=1
+# # a,b,c,1,2,'johan'
+
+
+# # # Python Standard Data type 
+# # # 1.numbers
+# # # 2. string
+# # # 3. list
+# # # 4.tuple
+# # # 5. dictionary
+
+# # #  python numbers
+# #     # 1. int
+# #     # 2. long
+# #     # 3. float
+# #     # 4. complex
+# # # Examples
+# # var1 = 1
+# # var2 = 10.5
+
+# # # python string 
+
+# # # example 
+# # str = 'hello wolrd '
+# # print(str[0])
+# # print(str[2:5])
+
+# # # python list
+# # list = ['abdi' , 7765 , 6789 , 'suldan' , 9876]
+# # print(list[1:3])
+
+# # # python tuple 
+# # tuple = ('abdi' , 7654 , 234, 'Sihaam' , 34.8)
+# # print(list[1:3])
+
+# # # python dictionary 
+# # dict = {'name': 'johan' , 'code':7643 , 'dept': 'sales'}
+# # print(dict['name'])
+# #                                     # End lesson 1.1
+# # # frist python program 
+# # # exm
+# # print("Hello Eng. Gabaxey ")
+
+# # # python  Keywords
+# # # . and , as , assert . break , class , continue , def and more.
+
+# # # list and identation 
+
+# # # exm
+# # if True:
+# #     print("this is inside the block")
+# # print("this is out side the block")
+
+# # #  multi-line Statements 
+
+# # # exam
+# # item_one = 12;
+# # item_two = 22;
+# # item_three = 33;
+# # total = item_one +\
+# #       item_two + \
+# #         item_three 
+
+# # # Quotation in python 
+
+# # word = 'word'
+# # sentence = "this is a sentence "
+# # paragraph = "this is a paragraph "
+
+# # # coments in python 
+
+# # #  this is a comment 
+# # print("hello world") # this is another comment
+
+# # #  Waiting for the user 
+# # # exam
+
+# # input("press enter to exist")
+# #                             # End lesson 1
+# # # data type conversion overview 
+# # int(), float(), str(), eval(), 
+# # # exam
+ 
+# # convertion int to str
+# # x = ('5')
+# # y = int(x)
+# # print(type(x))
+
+# # convertion str to float
+# # x = ('5.3')
+# # y = float(x)
+# # print(x)
+# # print(type(y))
+
+# #  confert int to str
+# x = 3
+# y = str(x) 
+# print(y)
+# print(type(y)) 
+
+# # convert eval to str 
+
+# x = '3+5'
+# y = eval(x)
+# print(y)
+
+# #  tuple , list , dictionary 
+
+# # convert list to tuple 
+tple = ('caasho ' , ' ali ')
+badal = list(tple)
+print(type(badal))
+
+
+#  convert lis to tuple 
+lst = ['sihaam ' , 'suldaan ', 'amal']
+badal = tuple(lst)
+print(type(badal))
+
+#  convert dictionary to list 
+
+dict = {1:'a', 2:'b' , 3:'sihaam'}
+badal = list(dict)
+print(dict)
+
+# chr(),ord(), hex(), oct()
+
+#  chr
+y = chr(65)
+print(y)
+
+#  ord 
+x = ord('A')
+print(x)
+
+# hex 
+
+z =hex(345)
+print(z)
+
+# oct 
+p = oct(9)
+print(p)
+
+
